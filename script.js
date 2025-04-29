101b2a32f97a0b8dd839a188d070263fe9d28135
Eduardo-salamanca <justinkwok800@gmail.com>
2025-04-29 16:36:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const menusOtak = {
+  "Day 1": {
+    "Menu 1": `Nasi Merah + Dada Ayam Panggang + Tumis Brokoli + Susu<br><br>
+Kalori: 520 kcal<br>Karbohidrat: 45 g<br>Protein: 38 g<br>Lemak: 18 g<br><br>
+<b>Cara Membuat:</b><br>
+- Masak nasi merah hingga matang.<br>
+- Lumuri dada ayam dengan garam, lada, panggang hingga matang.<br>
+- Tumis brokoli dengan bawang putih sedikit minyak.<br>
+- Sajikan bersama segelas susu.`,
+    "Menu 2": `Roti Gandum + Telur Rebus + Salad Sayur<br><br>
+Kalori: 450 kcal<br>Karbohidrat: 40 g<br>Protein: 22 g<br>Lemak: 15 g<br><br>
+<b>Cara Membuat:</b><br>
+- Rebus telur hingga matang.<br>
+- Sajikan dengan roti gandum dan salad sayur segar.`,
+    "Menu 3": `Quinoa + Salmon Panggang + Sayur Kukus<br><br>
+Kalori: 580 kcal<br>Karbohidrat: 35 g<br>Protein: 40 g<br>Lemak: 25 g<br><br>
+<b>Cara Membuat:</b><br>
+- Masak quinoa hingga empuk.<br>
+- Panggang salmon dengan minyak zaitun.<br>
+- Kukus wortel, buncis, dan jagung.`,
+    "Snack": `Yogurt Greek + Madu<br><br>
+Kalori: 150 kcal<br>Karbohidrat: 18 g<br>Protein: 12 g<br>Lemak: 2 g<br><br>
+<b>Cara Membuat:</b><br>
+- Campurkan yogurt Greek dengan madu alami.<br>
+- Sajikan dingin.`
+  },
+  // You can add "Day 2", "Day 3" the same way later
+};
